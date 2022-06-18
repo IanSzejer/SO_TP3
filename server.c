ee8b496c12a5b9784b780a0d0c2197af98b930bd
IanSzejer <ianszejer@gmail.com>
2022-06-18 12:06:57 -0300

    first commit

---
+++ server.c
@@ -0,0 +1,29 @@
+
+#define PORT 8080
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0))
+        == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_fd, SOL_SOCKET,
+                   SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
